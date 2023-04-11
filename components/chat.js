899e36a645f59414c9aaf94af32cce49526482f6
johngibbs15 <john.gibbs901@gmail.com>
2023-04-11 16:07:08 -0400

    Chat UI

--- components/chat.js
+++ components/chat.js
@@ -1,26 +1,61 @@
-import { StyleSheet, View, Text } from 'react-native';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = ({ route, navigation }) => {
-    const { name, color } = route.params;
+    const { name } = route.params;
+    const [messages, setMessages] = useState([]);
+    const onSend = (newMessages) => {
+        setMessages((previousMessages) =>
+            GiftedChat.append(previousMessages, newMessages)
+        );
+    };
+
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any',
+                },
+            },
+            {
+                _id: 2,
+                text: 'This is a system message',
+                createdAt: new Date(),
+                system: true,
+            },
+        ]);
+    }, []);
 
     useEffect(() => {
         navigation.setOptions({ title: name });
     }, []);
 
     return (
-        <View style={[styles.container, { backgroundColor: color }]}>
-            <Text>Hello {name}!</Text>
+        <View style={styles.container}>
+            <GiftedChat
+                messages={messages}
+                onSend={(messages) => onSend(messages)}
+                user={{
+                    _id: 1,
+                }}
+            />
+            {Platform.OS === 'ios' ? (
+                <KeyboardAvoidingView behavior="padding" />
+            ) : null}
         </View>
     );
 };
 
-export default Chat;
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
     },
 });
+
+export default Chat;
